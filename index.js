d2ac919ad4417d30f9a4df810ca8d26915ba2417
indrajith76 <indrajithgoswami@gmail.com>
2024-06-05 22:34:20 +0600

    adding features

--- index.js
+++ index.js
@@ -57,6 +57,18 @@ async function run() {
       res.send(result);
     });
 
+    app.post("/services", async (req, res) => {
+      const data = req.body;
+      const result = await servicesCollection.insertOne(data);
+      res.send(result);
+    });
+
+    app.delete("/services/:id", async (req, res) => {
+      const query = { _id: new ObjectId(req.params.id) };
+      const result = await servicesCollection.deleteOne(query);
+      res.send(result);
+    });
+
     app.delete("/myOrderList/:id", async (req, res) => {
       const query = { _id: new ObjectId(req.params.id) };
       const result = await allOrderRequestCollection.deleteOne(query);
@@ -67,11 +79,14 @@ async function run() {
       const email = req.params.email;
       const result = await allOrderRequestCollection
         .find({ email: email })
-        .toArray(); 
+        .toArray();
       res.send(result);
     });
 
-    app.get("/AllOrderList", async (req, res) => {});
+    app.get("/AllOrderList", async (req, res) => {
+      const result = await allOrderRequestCollection.find({}).toArray();
+      res.send(result);
+    });
 
     app.post("/AllOrderList", async (req, res) => {
       const {
